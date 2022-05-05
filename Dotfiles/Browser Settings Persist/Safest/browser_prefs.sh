53058acc8c1166b69e922728959025d49c08f1f4
S0yf00d.exe <104031749+S0yf00d-exe@users.noreply.github.com>
2022-05-05 08:11:00 +0000

    Create browser_prefs.sh

---
+++ Dotfiles/Browser Settings Persist/Safest/browser_prefs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir -p Persistent/scripts/
+
+cd Persistent/scripts/
+
+touch browser-preferences.sh
+echo '#!/bin/bash 
+echo "user_pref(\"extensions.torbutton.security_slider\", 1);
+user_pref(\"privacy.prioritizeonions.enabled\", true);
+user_pref(\"accessibility.force_disabled\", 1);
+user_pref(\"browser.toolbars.bookmarks.visibility\", \"always\");
+user_pref(\"extensions.torbutton.noscript_inited\", true);
+user_pref(\"permissions.default.camera\", 2);
+user_pref(\"permissions.default.desktop-notification\", 2);
+user_pref(\"permissions.default.geo\", 2);
+user_pref(\"permissions.default.microphone\", 2);
+user_pref(\"permissions.default.xr\", 2);
+user_pref(\"media.autoplay.default\", 5);
+user_pref(\"webgl.disabled\", true);" >> /home/amnesia/.tor-browser/profile.default/user.js' | tee -a Persistent/scripts/browser-preferences.sh
+
+chmod +x Persistent/scripts/browser-preferences.sh
+mkdir -p /live/persistence/TailsData_unlocked/dotfiles/.config/autostart/
+cd /live/persistence/TailsData_unlocked/dotfiles/.config/autostart/
+touch browser-preferences.desktop
+echo '[Desktop Entry]
+Name=Browser Prefernces
+Type=Application
+Exec=/home/amnesia/Persistent/scripts/browser-preferences.sh
+Terminal=false
+X-GNOME-Autostart-enabled=true' | tee -a /live/persistence/TailsData_unlocked/dotfiles/.config/autostart/browser-preferences.desktop
