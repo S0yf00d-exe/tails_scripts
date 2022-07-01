d98e5ecf16dd0f0c841bb11b1151055c0622ec03
S0yf00d.exe <104031749+S0yf00d-exe@users.noreply.github.com>
2022-07-01 04:06:12 +0000

    Create no-homepage_nojava.sh

---
+++ Dotfiles/Browser Settings Persist/Vanilla/Safest/no-homepage_nojava.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p /home/amnesia/Persistent/scripts/
+
+cd /home/amnesia/Persistent/scripts/
+
+touch browser-preferences.sh
+echo '#!/bin/bash 
+echo "user_pref(\"extensions.torbutton.security_slider\", 1);
+user_pref(\"browser.startup.homepage\", \"about:tor\");
+user_pref(\"privacy.prioritizeonions.enabled\", true);
+user_pref(\"browser.toolbars.bookmarks.visibility\", \"always\"); 
+user_pref("javascript.enabled", false);" >> /home/amnesia/.tor-browser/profile.default/user.js' | tee -a /home/amnesia/Persistent/scripts/browser-preferences.sh
+
+chmod +x /home/amnesia/Persistent/scripts/browser-preferences.sh
+mkdir -p /live/persistence/TailsData_unlocked/dotfiles/.config/autostart/
+cd /live/persistence/TailsData_unlocked/dotfiles/.config/autostart/
+touch browser-preferences.desktop
+echo '[Desktop Entry]
+Name=Browser Preferences
+Type=Application
+Exec=/home/amnesia/Persistent/scripts/browser-preferences.sh
+Terminal=false
+X-GNOME-Autostart-enabled=true' | tee -a /live/persistence/TailsData_unlocked/dotfiles/.config/autostart/browser-preferences.desktop
